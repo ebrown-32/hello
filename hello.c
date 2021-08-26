ae08994eb8e12357467309328262c367cdd2ffd9
ebrown-32 <ebrown32@huskers.unl.edu>
2021-08-26 16:07:12 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Author: Eli Brown
+ * Date: 2021/08/23
+ * Simple program that prints my name
+ */
+ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Eli Brown\n");
+    
+    return 0;
+    
+}
